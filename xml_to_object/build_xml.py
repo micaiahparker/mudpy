2a8f81700f4cf46bfd4bf4a505f1fda7d1fa4951
Micaiah Parker <micparke@nmu.edu>
2016-02-03 23:46:41 -0500

    setting up tools to build game out of xml

--- xml_to_object/build_xml.py
+++ xml_to_object/build_xml.py
@@ -1,18 +1,23 @@
 known = {}
 
 
+def add_known(cls):
+    known[cls.tag] = cls
+
+
 class Buildable:
     tag = 'buildable'
 
     def __init__(self, xml):
         self.contents = {}
+        name = ""
+
         for key in self.__dict__:
             if key in xml.attrib:
                 setattr(self, key, xml.attrib[key])
-        for child in xml:
-            if child.tag in known:
-                try:
-                    self.contents[child.tag][child.attrib['name']] = known[child.tag](child)
-                except KeyError:
-                    self.contents[child.tag] = {}
-                    self.contents[child.tag][child.attrib['name']] = known[child.tag](child)
+
+        self.post_init()
+
+    def post_init(self):
+        pass
+

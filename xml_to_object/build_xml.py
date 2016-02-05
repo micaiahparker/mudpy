6748ce20547a7712955915cd12089402af939d0c
Micaiah Parker <micparke@nmu.edu>
2016-02-04 19:03:13 -0500

    changed "look" to more complicated "examine"

--- xml_to_object/build_xml.py
+++ xml_to_object/build_xml.py
@@ -12,8 +12,27 @@ def make_item(item):
     return None
 
 
+def list_dict(adict):
+    ret = "\n"
+    for arg in adict:
+        ret += "{}\n".format(str(arg))
+
+    return ret
+
+
+def content_rules(inst, content):
+    if content.tag in known:
+        content = make_item(content)
+        try:
+            inst.__dict__[content.tag][content.name] = content
+        except KeyError:
+            inst.__dict__[content.tag] = {}
+            inst.__dict__[content.tag][content.name] = content
+
+
 class Buildable:
     tag = 'buildable'
+    viewables = []
 
     def __init__(self, xml):
         self.contents = []
@@ -24,20 +43,24 @@ class Buildable:
                 setattr(self, key, xml.attrib[key])
 
         for child in xml:
-            self.contents.append(make_item(child))
+            content_rules(self, child)
 
         self.post_init()
 
     def post_init(self):
         pass
 
-    def look(self, *args):
-        ret = '\n'
-        for arg in args:
-            for item in arg:
-                ret += "{}\n".format(item(str))
+    def examine(self):
+        ret = ""
+
+        for key in self.__dict__.keys():
+            if key in self.viewables:
+                ret += list_dict(self.__dict__[key])
+
         return ret
 
-add_known(Buildable)
+    def __str__(self):
+        return self.name + self.examine()
 
+add_known(Buildable)
 
--- xml_to_object/item_database.py
+++ xml_to_object/item_database.py
@@ -1,41 +1,12 @@
 from xml_to_object.build_xml import *
 
 
-class Rooms(Buildable):
-    def __init__(self, xml):
-        self.rooms = {}
-        super().__init__(xml)
-
-    def post_init(self):
-        for content in self.contents:
-            if content.tag == 'room':
-                self.rooms[content.name] = content
-                self.contents.remove(content)
-
-    def get_room(self, name):
-        try:
-            return self.rooms[name]
-        except KeyError:
-            return None
-
-
 class Room(Buildable):
-    def __init__(self, xml):
-        self.players = {}
-        self.items = {}
-        super().__init__(xml)
+    tag = 'room'
+    viewables = ['items', 'players']
 
-    def post__init__(self):
-        for content in self.contents:
-            if content.tag == 'player':
-                self.players[content.name] = content
-                self.contents.remove(content)
-            elif content.tag == 'item':
-                self.items[content.name] = content
-                self.contents.remove(content)
 
-    def look(self):
-        return super().look(self.players, self.items)
+
 
 
 
